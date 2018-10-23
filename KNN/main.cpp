70d96933107c8894b98959de8d960bec0b0745c6
ychen <ychen2167@gmail.com>
2018-10-23 18:18:08 +0800

    Initial Commit

---
+++ KNN/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KNN
+//
+//  Created by ychen on 2018/10/23.
+//  Copyright © 2018年 ychen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
